82184ece30510799c7638e6a8812132da3344f1a
istar112 <istarhf110809@gmail.com>
2026-01-23 16:35:40 +0100

    Creating authors class and creating an author

--- api_nestbook/app/main.py
+++ api_nestbook/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from app.routers import users
 from app.routers import books
+from app.routers import authors
 
 # New instance of FastAPI
 app = FastAPI(debug=True)
@@ -9,6 +10,7 @@ app = FastAPI(debug=True)
 # Including our router in app
 app.include_router(users.router)
 app.include_router(books.router)
+app.include_router(authors.router)
 
 
 # Ruta home
