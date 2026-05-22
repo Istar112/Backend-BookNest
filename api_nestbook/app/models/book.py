a1cdcba31d988647a6001d3db95542ff4ed5d1cc
vicferbal <victorferrandez94@gmail.com>
2026-05-22 22:59:44 +0200

    fix(books): Add desired to book

--- api_nestbook/app/models/book.py
+++ api_nestbook/app/models/book.py
@@ -16,13 +16,14 @@ class BookDb(BookBase):
     id:int
 
 class BookUpdate(BaseModel):
-    isbn: Optional[str]
-    title: Optional[str]
-    category: Optional[str]
-    total_pages: Optional[int]
-    publication_date: Optional[date]
-    purchased: Optional[bool]
-    cover_image: Optional[str]
+    isbn: Optional[str] = None
+    title: Optional[str] = None
+    category: Optional[str] = None
+    total_pages: Optional[int] = None
+    publication_date: Optional[date] = None
+    purchased: Optional[bool] = None
+    cover_image: Optional[str] = None
+    desired: Optional[bool] = None
 
 class Book_authorDb(BaseModel):
     id: int
